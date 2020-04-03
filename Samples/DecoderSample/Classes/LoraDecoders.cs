6c9ffd032fab9d511e9cb8b4f5f85a5b2463eb91
Michael Gerfen <gerfen@hotmail.com>
2020-04-03 13:27:36 -0700

    Added decoder for Decentlab weather station

--- Samples/DecoderSample/Classes/LoraDecoders.cs
+++ Samples/DecoderSample/Classes/LoraDecoders.cs
@@ -6,6 +6,17 @@ namespace SensorDecoderModule.Classes
     using System;
     using System.Text;
     using Newtonsoft.Json;
+    using DecentlabWaterLevelSensorDecoder = Evolution.IoT.LoraWan.Decoders.Decentlab.WaterLevelSensorDecoder;
+    using DecentlabWeatherStationSensorDecoder = Evolution.IoT.LoraWan.Decoders.Decentlab.WeatherStationSensorDecoder;
+    using LansitecTemperatureHumidityDecoder = Evolution.IoT.LoraWan.Decoders.Lansitec.TemperatureHumidityDecoder;
+    using NetvoxTemperatureHumidityDecoder = Evolution.IoT.LoraWan.Decoders.Netvox.TemperatureHumidityDecoder;
+    using SensumTemperatureHumidityDecoder = Evolution.IoT.LoraWan.Decoders.Sensum.TemperatureHumidityDecoder;
+    using SensumSoilMoistureDecoder = Evolution.IoT.LoraWan.Decoders.Sensum.SoilMoistureDecoder;
+    using DigitalMatterOysterDecoder = Evolution.IoT.LoraWan.Decoders.DigitalMatter.OysterDecoder;
+    using UsraLinkUC1152Decoder = Evolution.IoT.LoraWan.Decoders.UrsaLink.Uc1152Decoder;
+    using UsraLinkUC1114Decoder = Evolution.IoT.LoraWan.Decoders.UrsaLink.Uc1114Decoder;
+    using StregaSmartValveDecoder = Evolution.IoT.LoraWan.Decoders.Strega.SmartValveDecoder;
+    using Evolution.IoT.LoraWan.Decoders.TTN;
 
     internal static class LoraDecoders
     {
@@ -49,6 +60,63 @@ namespace SensorDecoderModule.Classes
             // Return a JSON string containing the decoded data
             return JsonConvert.SerializeObject(resultObject);
 
+        }
+
+              // Add decoders methods here...
+        private static string DecoderTheThingsNodeSensor(string devEUI, byte[] payload, uint fport) 
+        {
+            return ThingsNodeDecoder.Decode(devEUI, payload, fport);
+        }
+
+         private static string DecoderLansitecTemperatureHumiditySensor(string devEUI, byte[] payload, uint fport) 
+        {
+            return LansitecTemperatureHumidityDecoder.Decode(devEUI, payload, fport);
+        }
+
+        private static string DecoderDecentlabWaterLevelSensor(string devEUI, byte[] payload, uint fport) 
+        {
+            return DecentlabWaterLevelSensorDecoder.Decode(devEUI, payload, fport);
+        }
+
+        private static string DecoderDecentlabWeatherStationSensor(string devEUI, byte[] payload, uint fport) 
+        {
+            return DecentlabWeatherStationSensorDecoder.Decode(devEUI, payload, fport);
+        }
+  
+
+       private static string DecoderNetvoxTemperatureHumidity(string devEUI, byte[] payload, uint fport) 
+        {
+            return NetvoxTemperatureHumidityDecoder.Decode(devEUI, payload, fport);
+        }
+
+        private static string DecodeSensumTemperatureHumidity(string devEUI, byte[] payload, uint fport)
+        {
+           return SensumTemperatureHumidityDecoder.Decode(devEUI, payload, fport);
+        }
+
+        private static string  DecodeSensumSoilMoisture(string devEUI, byte[] payload, uint fport)
+        {
+            return SensumSoilMoistureDecoder.Decode(devEUI, payload, fport);
+        }
+
+        private static string DecodeDigitalMatterOyster(string devEUI, byte[] payload, uint fport)
+        {
+            return DigitalMatterOysterDecoder.Decode(devEUI, payload, fport);
+        }
+
+        private static string DecodeUrsaLinkUc1152(string devEUI, byte[] payload, uint fport)
+        {
+            return UsraLinkUC1152Decoder.Decode(devEUI, payload, fport);
+        }
+
+         private static string DecodeUrsaLinkUc1114(string devEUI, byte[] payload, uint fport)
+        {
+            return UsraLinkUC1114Decoder.Decode(devEUI, payload, fport);
+        }
+
+        private static string DecodeStregaSmartValve(string devEUI, byte[] payload, uint fport)
+        {
+            return StregaSmartValveDecoder.Decode(devEUI, payload, fport);
         }
     }
 }
